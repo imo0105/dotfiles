83c56c6e3169200f59d6d7100f9462b5da58b5d2
Shota Sato <imo0105b@gmail.com>
2019-08-03 21:34:35 +0900

    Add goenv settings

--- .config/fish/functions/exports.fish
+++ .config/fish/functions/exports.fish
@@ -4,6 +4,11 @@ set -x XDG_CONFIG_HOME $HOME/.config
 # gopath
 set -x GOPATH $HOME/go
 
+## goenv 
+set -x GOENV_ROOT $HOME/.goenv
+set -x PATH $GOENV_ROOT/bin $PATH 
+goenv init - | source
+
 # user path
 set -x PATH $PATH $HOME/bin
 
